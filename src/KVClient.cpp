5506bb9cb7218bb59392e8b6e90e5467ac413d73
Fenil Mehta <fenilgmehta@gmail.com>
2020-11-04 11:52:12 +0530

    Implement KVClientLibrary, KVClient & Add sample file for load testing
    - client_request_001.txt file shall be used when doing load testing of the server

--- src/KVClient.cpp
+++ src/KVClient.cpp
@@ -1,1 +1,117 @@
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <fstream>
+#include <sys/stat.h>
+
+#include "MyDebugger.hpp"
 #include "KVClientLibrary.hpp"
+#include "KVMessage.hpp"
+
+using namespace std;
+
+
+/* Used for load testing */
+void start_sending_requests(const vector<KVMessage> &dataset, const char *server_ip = "127.0.0.1", const char *server_port = "12345") {
+#ifdef DEBUGGING_ON
+    log_info("-----+-----+-----");
+    log_info(dataset.size());
+    for (auto i: dataset) {
+        log_info(string("") + to_string((int) i.status_code) + " " + i.key + " " + i.value);
+    }
+    log_info("-----+-----+-----");
+#endif
+
+    // TODO: replace static values with "server_ip" and "server_port"
+    ClientServerConnection connection(server_ip, server_port);
+    int request_number = 1;
+    for (auto &i: dataset) {
+        log_info(string("Working on Request Number = ") + to_string(request_number));
+        ++request_number;
+
+        // No other case is possible because they are handled while reading the dataset file
+        switch (i.status_code) {
+            case 1:
+                connection.GET(i);
+                if (not equal(i.value, i.value + 256, connection.resultValue)) {
+                    log_error("*** Server response was not consistent with what was expected if this is the only client updating the server, or if all clients are making request for different keys ***");
+                }
+                break;
+            case 2:
+                connection.PUT(i);
+                break;
+            case 3:
+                connection.DELETE(i);
+                break;
+        }
+    }
+}
+
+
+// REFER: https://stackoverflow.com/questions/12774207/fastest-way-to-check-if-a-file-exist-using-standard-c-c11-c
+inline bool file_exists(const char *name) {
+    struct stat buffer{};
+    return (stat(name, &buffer) == 0);
+}
+
+vector<KVMessage> load_client_request_dataset(char *filename) {
+    if (not file_exists(filename)) {
+        log_error(string("") + "FILE not found: \"" + filename + "\"");
+        exit(1);
+    }
+    ifstream fileReader;
+    fileReader.open(filename, ios::in);
+
+    // REFER: https://stackoverflow.com/questions/3640852/difference-between-file-is-open-and-file-fail
+    if (fileReader.fail() || (not fileReader.is_open())) {
+        log_error("File opening failed");
+        exit(2);
+    }
+
+    // REFER: https://www.tutorialspoint.com/cplusplus/cpp_files_streams.htm
+    uint32_t requestCount = 0;
+    fileReader >> requestCount;
+    log_info(string("Request Count = ") + to_string(requestCount));
+
+    vector<KVMessage> dataset(requestCount);
+    uint32_t request_type;
+    struct KVMessage temp;
+    for (int i = 0; i < requestCount; ++i) {
+        // Request Codes: 1=GET, 2=PUT, 3=DELETE
+        fileReader >> request_type;
+        if (not(1 <= request_type && request_type <= 3)) {
+            log_error("Invalid value of Request Code = " + to_string(request_type));
+            fileReader.close();
+            exit(3);
+        }
+
+        temp.status_code = request_type;
+        fileReader >> temp.key;
+        if (request_type == 2) fileReader >> temp.value;  // "Value" is only required for PUT requests
+        dataset[i] = temp;
+    }
+
+    fileReader.close();
+    return dataset;
+}
+
+int main(int argc, char *argv[]) {
+    // REFER: https://www.geeksforgeeks.org/command-line-arguments-in-c-cpp/
+    if (argc == 4) {
+        log_info("Loading client requests dataset...", true);
+        vector<KVMessage> dataset = load_client_request_dataset(argv[1]);
+        log_info("Loading successfully complete\n");
+
+        // TODO: comment this when using Bash/Python script to launch multiple clients
+        // REFER: https://stackoverflow.com/questions/903221/press-enter-to-continue
+        cout << "Press Enter to Continue";
+        cin.ignore();
+
+        // TODO: add technique to measure execution time
+        start_sending_requests(dataset, argv[2], argv[3]);
+        return 0;
+    }
+
+    // TODO
+    return 0;
+}
--- src/KVClientLibrary.hpp
+++ src/KVClientLibrary.hpp
@@ -1,4 +1,141 @@
 #ifndef PA_4_KEY_VALUE_STORE_KVCLIENTLIBRARY_HPP
 #define PA_4_KEY_VALUE_STORE_KVCLIENTLIBRARY_HPP
 
+#include <string>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <cctype>
+
+#include "MyDebugger.hpp"
+#include "KVMessage.hpp"
+
+// REFER: https://www.bogotobogo.com/cplusplus/sockets_server_client.php
+// REFER: https://stackoverflow.com/questions/1593946/what-is-af-inet-and-why-do-i-need-it
+
+/*
+ *  Code to test the below thing
+ *
+
+    const char REQUEST_FAILURE_MSG[] = "Key not found";
+    char arr[100] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+    std::copy(std::begin(REQUEST_FAILURE_MSG), std::end(REQUEST_FAILURE_MSG), arr);
+    std::cout << "'" << arr << "'" << "\n";
+    std::cout << (int) arr[12] << ", " << (int) arr[13] << ", " << (int) arr[14] << ", " << (int) arr[15] << "\n";
+
+    const char REQUEST_FAILURE_MSG_2[] = "Key not found\0";
+    char arr_2[100] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+    std::copy(std::begin(REQUEST_FAILURE_MSG_2), std::end(REQUEST_FAILURE_MSG_2), arr_2);
+    std::cout << "'" << arr_2 << "'" << "\n";
+    std::cout << (int) arr_2[12] << ", " << (int) arr_2[13] << ", " << (int) arr_2[14] << ", " << (int) arr_2[15] << "\n";
+
+ * */
+const static char REQUEST_FAILURE_MSG[] = "Key not found";  // Internally this has length 14 and last character (i.e. index = 13) is '\0'
+
+struct ClientServerConnection {
+    int socketFD;  // used to communicated with the Server over the socket
+    uint8_t resultStatusCode;
+    char resultValue[256];
+
+    ClientServerConnection(const char *serverIP, const char *serverPort) : resultStatusCode{}, resultValue{} {
+        // REFERRED: B.E. Computer Network's file transfer program
+
+        // REFER: https://stackoverflow.com/questions/5815675/what-is-sock-dgram-and-sock-stream
+        socketFD = socket(AF_INET, SOCK_STREAM, 0);
+
+        if (socketFD < 0) {
+            log_error("Unable to open the socket");
+            exit(4);
+        }
+
+        struct hostent *server;
+        server = gethostbyname(serverIP);
+        if (server == nullptr) {
+            log_error("No such host");
+            exit(5);
+        }
+
+        struct sockaddr_in serv_addr{};
+        serv_addr.sin_family = AF_INET;
+        bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
+        serv_addr.sin_port = htons(atoi(serverPort));
+        // REFER: https://stackoverflow.com/questions/19207745/htons-function-in-socket-programing
+        // REFER: https://www.geeksforgeeks.org/converting-strings-numbers-cc/
+
+        // connect()
+        if (connect(socketFD, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+            log_error("Connection with the server failed");
+            exit(6);
+        }
+    }
+
+    ~ClientServerConnection() {
+        close(socketFD);
+    }
+
+#ifdef DEBUGGING_ON
+#define ASSERT_SUCCESS(rw_operation) \
+    if ((rw_operation) < 0) {        \
+        log_error(std::string("") + "Error reading/writing to the server: \"" + #rw_operation + "\""); \
+        log_error("EXITING (status=7)"); \
+        exit(7);                         \
+    }
+#else
+#define ASSERT_SUCCESS(rw_operation) rw_operation;
+#endif
+
+    void GET(const struct KVMessage &message) {
+        // 1 represents GET request
+        ASSERT_SUCCESS(write(socketFD, reinterpret_cast<const void *>("1"), 1))
+        ASSERT_SUCCESS(write(socketFD, reinterpret_cast<const void *>(message.key), 256))
+
+        ASSERT_SUCCESS(read(socketFD, reinterpret_cast<void *>(resultStatusCode), 1))
+        ASSERT_SUCCESS(read(socketFD, reinterpret_cast<void *>(resultValue), 256))
+        print_result_returned("GET");
+    }
+
+    void PUT(const struct KVMessage &message) {
+        // 2 represents PUT request
+        ASSERT_SUCCESS(write(socketFD, reinterpret_cast<const void *>("2"), 1))
+        ASSERT_SUCCESS(write(socketFD, reinterpret_cast<const void *>(message.key), 256))
+        ASSERT_SUCCESS(write(socketFD, reinterpret_cast<const void *>(message.value), 256))
+
+        ASSERT_SUCCESS(read(socketFD, reinterpret_cast<void *>(resultStatusCode), 1))
+        if (resultStatusCode == 240) {
+            ASSERT_SUCCESS(read(socketFD, reinterpret_cast<void *>(resultValue), 256))
+            std::copy(std::begin(REQUEST_FAILURE_MSG), std::end(REQUEST_FAILURE_MSG), resultValue);
+        }
+        print_result_returned("PUT");
+    }
+
+    void DELETE(const struct KVMessage &message) {
+        // 3 represents DELETE request
+        ASSERT_SUCCESS(write(socketFD, reinterpret_cast<const void *>("3"), 1))
+        ASSERT_SUCCESS(write(socketFD, reinterpret_cast<const void *>(message.key), 256))
+
+        ASSERT_SUCCESS(read(socketFD, reinterpret_cast<void *>(resultStatusCode), 1))
+        if (resultStatusCode == 240) {
+            ASSERT_SUCCESS(read(socketFD, reinterpret_cast<void *>(resultValue), 256))
+            std::copy(std::begin(REQUEST_FAILURE_MSG), std::end(REQUEST_FAILURE_MSG), resultValue);
+        }
+        print_result_returned("DELETE");
+    }
+
+    void print_result_returned(const char *operationName) {
+        if (resultStatusCode == 200) {
+            log_info(std::string(operationName) + ": was successful");
+        } else if (resultStatusCode == 240) {
+            log_warning(std::string(operationName) + ": resultStatusCode = 240");
+            log_warning(std::string("    ") + "--> Returned Messaged says: \"" + resultValue + "\"");
+        } else {
+            log_error("INVALID resultStatusCode = " + std::to_string((int) resultStatusCode), true, true);
+        }
+    }
+
+#undef ASSERT_SUCCESS
+
+};
+
 #endif // PA_4_KEY_VALUE_STORE_KVCLIENTLIBRARY_HPP
