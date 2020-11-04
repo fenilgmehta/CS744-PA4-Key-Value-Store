30042b497511d279141ef60d410af6034d9d82ef
Fenil Mehta <fenilgmehta@gmail.com>
2020-11-04 06:25:59 +0530

    Update to C++17, Add Custom Header as a Faster alternative to multiple malloc for small memory

---
+++ src/MyMemoryPool.hpp
@@ -0,0 +1,12 @@
+#ifndef PA_4_KEY_VALUE_STORE_MYMEMORYPOOL_HPP
+#define PA_4_KEY_VALUE_STORE_MYMEMORYPOOL_HPP
+
+#include <vector>
+#include <>
+
+struct MemoryPool {
+
+    MemoryPool() {}
+};
+
+#endif // PA_4_KEY_VALUE_STORE_MYMEMORYPOOL_HPP
