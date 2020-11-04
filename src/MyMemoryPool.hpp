ded8d0bd9a4d5530dd9e17fd86a20bb16742da1d
Fenil Mehta <fenilgmehta@gmail.com>
2020-11-05 05:24:30 +0530

    Implement MyMemoryPool, Add new comments and methods to MyVector

--- src/MyMemoryPool.hpp
+++ src/MyMemoryPool.hpp
@@ -1,12 +1,65 @@
 #ifndef PA_4_KEY_VALUE_STORE_MYMEMORYPOOL_HPP
 #define PA_4_KEY_VALUE_STORE_MYMEMORYPOOL_HPP
 
-#include <vector>
-#include <>
+#include <mutex>
+#include "MyVector.hpp"
+#include "MyDebugger.hpp"
 
+/*
+ * REFER: https://stackoverflow.com/questions/855763/is-malloc-thread-safe
+ *
+ *
+ *
+ * */
+template<typename T>
 struct MemoryPool {
+    size_t blockSize;
+    MyVector<T*> pool;
+    MyVector<T*> memoryBlockPointers;
+    std::mutex m;
 
-    MemoryPool() {}
+    explicit MemoryPool(size_t block_size) : blockSize{block_size}, pool(2 * block_size), memoryBlockPointers(8), m() {
+        allocate_one_block();
+    }
+
+    ~MemoryPool() {
+        for(int64_t i = 0; i < this->memoryBlockPointers.n; ++i) {
+            free(this->memoryBlockPointers.at(i));
+        }
+    }
+
+    T* acquire_instance() {
+        m.lock();
+        if(memoryBlockPointers.empty()) {
+            allocate_one_block();
+        }
+        T *instance_ptr = memoryBlockPointers.pop_back();
+        m.unlock();
+        return instance_ptr;
+    }
+
+    void release_instance(T* ptr) {
+        m.lock();
+        memoryBlockPointers.push_back(ptr);
+        m.unlock();
+    }
+
+private:
+    void allocate_one_block() {
+        memoryBlockPointers.push_back(reinterpret_cast<T *>(malloc(this->blockSize * sizeof(T))));
+        T *new_block_ptr = memoryBlockPointers.at_rev(0);
+
+        if(new_block_ptr == nullptr) {
+            // malloc failed
+            log_error("malloc failed in \"MemoryPool.allocate_one_block()\" method");
+            log_error("Exiting (status=61)");
+            exit(61);
+        } else {
+            for(int64_t i = 0; i < this->blockSize; ++i) {
+                pool.push_back(new_block_ptr + i);
+            }
+        }
+    }
 };
 
 #endif // PA_4_KEY_VALUE_STORE_MYMEMORYPOOL_HPP
--- src/MyVector.hpp
+++ src/MyVector.hpp
@@ -4,19 +4,39 @@
 #include <cstdlib>
 #include <cstdint>
 
-// REFER: https://github.com/fenilgmehta/Misc-Programming/blob/master/src/MyVector.c
-
+/*
+ * REFER: https://github.com/fenilgmehta/Misc-Programming/blob/master/src/MyVector.c
+ *
+ * MyVector is 3 times faster than std::vector when inserting 10'00'00'000
+ * elements using push_back(...) function with initial capacity set to 10'000
+ *
+ * CAUTION: the constructor and destructor will NOT be called on actual "push_back"
+ *          and "pop_back" operation.
+ *        : the destructor will not be called for any object in the "MyVector" object
+ *          when "~MyVector()" is invoked
+ *        : datatype T should be safe to work with "malloc", "realloc" and "free"
+ *
+ * */
 template<typename T>
 struct MyVector {
     size_t n{}, nMax{};
     T *arr;
 
     /* Constructor */
-    explicit MyVector(const size_t n) : n{0}, nMax{n} {
-        this->arr = static_cast<T *>(malloc(n * sizeof(T)));
+    explicit MyVector(const size_t n_capacity) : n{0}, nMax{n_capacity} {
+        this->arr = static_cast<T *>(malloc(n_capacity * sizeof(T)));
     }
 
     inline T &at(const size_t idx) {
+        return this->arr[idx];
+    }
+
+    /* Reverse indexing, i.e. 0 points to last element(i.e. index = n-1) of MyVector */
+    inline T &at_rev(const size_t idx) {
+        return this->arr[n-1-idx];
+    }
+
+    inline const T &at(const size_t idx) const {
         return this->arr[idx];
     }
 
@@ -33,9 +53,9 @@ struct MyVector {
     }
 
     /* RETURNS: true if push_back was successful */
-    bool push_back(T val) {
+    bool push_back(const T &val) {
         if (this->n == this->nMax) {
-            if (not(this->resize(this->nMax * 2))) {
+            if (not(this->resize(this->nMax * 4))) {
                 /* if resize fails */
                 return false;
             }
@@ -49,14 +69,23 @@ struct MyVector {
     inline T &pop_back() {
         if (this->n <= 1) this->n = 0;
         else this->n -= 1;
+
+        // REFER: https://www.geeksforgeeks.org/possible-call-constructor-destructor-explicitly/
+        // explicitly call the destructor
+        this->arr[this->n].~T();
         return this->arr[this->n];
     }
 
-    /* RETURNS: the last element popped */
+    /* RETURNS: the last element popped
+     * CAUTION: the destructor will only be called for the last element popped
+     * */
     inline T &pop_back(const size_t popCount) {
         if (popCount <= 0) return this->arr[0];
         if (this->n <= popCount) this->n = 0;
         else this->n -= popCount;
+
+        // explicitly call the destructor for the last element popped
+        this->arr[this->n].~T();
         return this->arr[this->n];
     }
 
@@ -73,6 +102,14 @@ struct MyVector {
             this->nMax = newSize;
         }
         return arrNew != NULL;
+    }
+
+    [[nodiscard]] inline size_t size() const {
+        return n;
+    }
+
+    [[nodiscard]] inline bool empty() const {
+        return this-n == 0;
     }
 
     /* Destructor */
